133c8e50d1a7e0cbb46c76b4b5aa90029076530e
Liz Shulman <corgipower@hotmail.com>
2020-06-13 12:17:04 -0400

    header done

--- components/Header/index.js
+++ components/Header/index.js
@@ -11,4 +11,26 @@
 // Use your function to create a header
 // and append it to the DOM inside the div.header-container
 
-function Header() {}
+function Header() {
+    const header = document.createElement('div');
+    const date = document.createElement('span');
+    const headingOne = document.createElement('h1');
+    const temp = document.createElement('span');
+
+    date.textContent = 'SMARCH 28, 2019';
+    headingOne.textContent = 'Lambda Times';
+    temp.textContent = '98°';
+
+    header.classList.add('header');
+    date.classList.add('date');
+    temp.classList.add('temp');
+
+    header.appendChild(date);
+    header.appendChild(headingOne);
+    header.appendChild(temp);
+
+    return header;
+}
+
+const headerContainer = document.querySelector('.header-container');
+headerContainer.append(Header());
